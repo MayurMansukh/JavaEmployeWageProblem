a9a1cf3f1f01676ba285fca5d6f100c6919dc7fc
MayurMansukh <Mansukhmayur99@gmail.com>
2021-02-23 08:06:40 +0530

    part time employee wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,62 +2,32 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Employee wage computation");
 		
-		int IS_PRESENT=1;
-		double wagePerHour=20;
-		double fullDay=8;	
-		double empWage;
-		double empCheck=Math.floor(Math.random() * 10 ) %2;
+	int IS_PRESENT=1;
+				double wagePerHour=20;
+				double fullDay=8;
+				double halfDay=4;	
+				double empWage;
+				double empCheck=Math.floor(Math.random() * 10 ) %2;
+	
+
+				if (empCheck==IS_PRESENT){
+				int IS_FULLTIME=1;
+				double fullTimeCheck=Math.floor(Math.random()*10)%2;
+			
+				System.out.println("Employee is present");
+
+				if (fullTimeCheck==IS_FULLTIME){
+				empWage=wagePerHour*fullDay;
+				System.out.println("Employee works fulltime and Employee wage is: "+ empWage);
+				}
+				else {
+				empWage=wagePerHour*halfDay;
+                                System.out.println("Employee works parttime and Employee wage is: "+ empWage);
+				}
+			}
 		
 
-		if (empCheck==IS_PRESENT){
-			System.out.println("Employee is present");
-			empWage=wagePerHour*fullDay;
-			System.out.println("Employee wage is: "+ empWage);
-		}
 		else{
 			System.out.println("Employee is absent");
 		}
 	}
-}
-public class EmployeeWage {
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Employee wage computation");
-		
-		int IS_PRESENT=1;
-		double wagePerHour=20;
-		double fullDay=8;	
-		double empWage;
-		double empCheck=Math.floor(Math.random() * 10 ) %2;
-		
-
-		if (empCheck==IS_PRESENT){
-			System.out.println("Employee is present");
-			empWage=wagePerHour*fullDay;
-			System.out.println("Employee wage is: "+ empWage);
-		}
-		else{
-			System.out.println("Employee is absent");
-		}
-	}
-}
-public class EmployeeWage {
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Employee wage computation");
-		
-		int IS_PRESENT=1;
-		double wagePerHour=20;
-		double fullDay=8;	
-		double empWage;
-		double empCheck=Math.floor(Math.random() * 10 ) %2;
-		
-
-		if (empCheck==IS_PRESENT){
-			System.out.println("Employee is present");
-			empWage=wagePerHour*fullDay;
-			System.out.println("Employee wage is: "+ empWage);
-		}
-		else{
-			System.out.println("Employee is absent");
-		}
-	}
-}
